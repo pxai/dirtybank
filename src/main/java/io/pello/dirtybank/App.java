9b6238ba45a648fe8745d43e39ed638f61d6e655
pxai <pello_altadill@cuatrovientos.org>
2017-06-23 19:42:46 +0200

    ok, first commit again

---
+++ src/main/java/io/pello/dirtybank/App.java
@@ -0,0 +1,62 @@
+package io.pello.dirtybank;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+/**
+ * App
+ * Main entry Point for the dirtybank app
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Connection conn = null;
+
+        do {
+            System.out.println("Welcome to the Dirty Bank");
+            Scanner reader = new Scanner(System.in);
+            String line = "";
+            int option = 0;
+
+
+
+            // Check on db
+            try {
+            	Class.forName("org.sqlite.JDBC");
+                String url = "jdbc:sqlite:dirtybank.db";
+                // create a connection to the database
+                 conn = DriverManager.getConnection(url);
+
+
+
+            // Validation /////
+            System.out.println("Please enter your account name");
+            String account = reader.nextLine();
+            System.out.println("Please enter your password");
+            String password = reader.nextLine();
+
+            // Check if user exists
+            PreparedStatement statement = conn.prepareStatement("SELECT * FROM customer where login=? and password=?");
+            statement.setString(1, account);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
+            
+            if (resultSet.next()) {
+            	System.out.println("Welcome Mr. " + resultSet.getString("name"));
+            } else {
+            	System.err.println("Login incorrect");
+            }
+
+            } catch (Exception e) {
+                System.err.println("Error connecting to db : " + e.getMessage());
+                e.printStackTrace();
+            }
+
+        } while (true);
+
+    }
+}
